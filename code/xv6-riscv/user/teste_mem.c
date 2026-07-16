781a3f3a51e22a2014dfabfd70910ed5a22cc699
Jailce Fernanda <jailce@gmail.com>
2026-07-15 22:47:37 -0300

    Update teste_mem.c

--- code/xv6-riscv/user/teste_mem.c
+++ code/xv6-riscv/user/teste_mem.c
@@ -5,18 +5,24 @@
 int
 main(int argc, char *argv[])
 {
+	// Teste da Syscall getfreemem
 	uint64 antes, depois;
 	printf("Iniciando teste da Syscall getfreemem\n");
+
 	antes = getfreemem();
+	// Força a alocação de memória para testar a liberação
 	printf("Memoria livre ANTES da alocacao: %lu bytes\n", antes);
 	printf("Alocando memoria...\n");
 	char *mem1 = malloc(1024 * 1024);
 	char *mem2 = malloc(2 * 1024 * 1024);
+
 	depois = getfreemem();
+	// Teste da liberacao de memoria
 	printf("Memoria livre DEPOIS da alocacao: %lu bytes\n", depois);
 	printf("Liberando memoria...\n");
 	free(mem1);
 	free(mem2);
+	
 	exit(0);
 }
 
